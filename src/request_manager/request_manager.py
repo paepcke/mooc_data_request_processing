fd8b7be848496f230908ac8824df874ba7335a6a
Andreas Paepcke <paepcke@cs.stanford.edu>
2017-04-12 14:35:10 -0700

    Adjusted indentation to 4 spaces.

--- src/request_manager/request_manager.py
+++ src/request_manager/request_manager.py
@@ -22,42 +22,42 @@ class DataRequestManager(object):
         '''
         
     def get_request_survey(self):
-      pass
+        pass
     
     def get_req_info_from_admin(self):
-      '''
-      Present Web page, prefilled form
-      with info from request. Kathy can
-      fill in additions (courses shared).
-      Results returned from here.
-      '''
-      pass
+        '''
+        Present Web page, prefilled form
+        with info from request. Kathy can
+        fill in additions (courses shared).
+        Results returned from here.
+        '''
+        pass
     
     def save_qualtrics_info(self, qualtrics_request_url):
-      '''
-      Given the URL to a filled-in request form, 
-      retrieve the request form, parse the HTML,
-      create name-value pairs from each field, and
-      save to file. 
-      '''
-      req      = urllib2.Request(qualtrics_request_url)
-      response = urllib2.urlopen(req)
-      the_page = response.read()
-      print(the_page)
-      soup = BeautifulSoup(the_page, "html.parser")
-      # soup('span', {'class' : 'Data'}): array of spans in order
-      soup.fetch('span', {'class' : 'Data'})
-      
-      print(soup('Data'))
+        '''
+        Given the URL to a filled-in request form, 
+        retrieve the request form, parse the HTML,
+        create name-value pairs from each field, and
+        save to file. 
+        '''
+        req      = urllib2.Request(qualtrics_request_url)
+        response = urllib2.urlopen(req)
+        the_page = response.read()
+        print(the_page)
+        soup = BeautifulSoup(the_page, "html.parser")
+        # soup('span', {'class' : 'Data'}): array of spans in order
+        soup.fetch('span', {'class' : 'Data'})
+        
+        print(soup('Data'))
     
     def fill_in_dua_wordfile(self, courses):
-      pass
+        pass
     
     def check_prerequisites(self):
-      pass
+        pass
     
 if __name__ == '__main__':
   
-  data_manager = DataRequestManager()
-  data_manager.save_qualtrics_info('https://proxy.qualtrics.com/proxy/?url=https%3A%2F%2Fstanforduniversity.qualtrics.com%2FCP%2FReport.php%3FSID%3DSV_ahriYiMPjMnz56l%26R%3DR_ToMk7CUJZFhCeYh&token=yDx4ygoyrTjiYkQFyAHhwiycw5TiIHXIS5orJyc1z2Q%3D')
+                                                                                                        data_manager = DataRequestManager()
+                                                                                                        #data_manager.save_qualtrics_info('https://proxy.qualtrics.com/proxy/?url=https%3A%2F%2Fstanforduniversity.qualtrics.com%2FCP%2FReport.php%3FSID%3DSV_ahriYiMPjMnz56l%26R%3DR_ToMk7CUJZFhCeYh&token=yDx4ygoyrTjiYkQFyAHhwiycw5TiIHXIS5orJyc1z2Q%3D')
   
